b1031d24b0e1be027a8c319c52d252029c34ccfa
mihalacheD <danutza_dima88@yahoo.com>
2024-01-14 15:17:37 +0200

    nav bar done

--- litllelemon/src/App.js
+++ litllelemon/src/App.js
@@ -1,11 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Nav from'./Nav.js';
 
 function App() {
   return (
-    <div className="App">
-      homepage
-    </div>
+    <>
+    <Nav/>
+    </>
   );
 }
 
