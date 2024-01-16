1aedbf679652c20b6fb4a74e17dca6f187c2284f
mihalacheD <danutza_dima88@yahoo.com>
2024-01-16 18:06:26 +0200

    nav done

--- litllelemon/src/components/Nav.js
+++ litllelemon/src/components/Nav.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/Logo .svg'
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
   return (
-    <nav>
-      <a href='/'>
-        <img src={ logo } alt='logo'/>
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href='/' className='logo'>
+        <img src={logo} alt='logo'/>
       </a>
 
 
      {/* mobile nav bar*/}
 
-     <div>
+     <div className='menu-icon' onClick={toggleMenu}>
       <div className='bar'></div>
       <div className='bar'></div>
       <div className='bar'></div>
      </div>
+
+     {/*nav items*/}
+
+     <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+      <li>
+        <a href='/'>Home</a>
+      </li>
+      <li>
+        <a href='/'>About</a>
+      </li>
+      <li>
+        <a href='/'>Services</a>
+      </li>
+      <li>
+        <a href='/'>Menu</a>
+      </li>
+      <li>
+        <a href='/'>Reservations</a>
+      </li>
+      <li>
+        <a href='/'>Order Online</a>
+      </li>
+      <li>
+        <a href='/'>Login</a>
+      </li>
+     </ul>
     </nav>
-  )
+  );
 };
-export default Nav
+export default Nav;
