a888c8177ff9e16cfc44007a71efec70077556ba
mihalacheD <danutza_dima88@yahoo.com>
2024-01-18 15:49:45 +0200

    work done

--- litllelemon/src/components/Header.js
+++ litllelemon/src/components/Header.js
@@ -1,31 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import bannerImg from '../images/restauranfood.jpg'
+import bannerImg from "../images/restauranfood.jpg";
 
 const Header = () => {
   return (
-    <header className="header" >
+    <header className="header">
       <section>
-        {/* banner texts*/}
         <div className="banner">
           <h2>Little Lemon</h2>
           <h3>Chicago</h3>
           <p>
-            We are a family owned Mediterranean restaurant, focused on traditional
-            recipes served whith a modern twist.
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional recipes servred with a modern twist.
           </p>
           <Link to="/booking">
             <button aria-label="On Click">Reserve Table</button>
-            </Link>
+          </Link>
         </div>
-
-        {/* banner image*/}
-
         <div className="banner-img">
-          <img src={bannerImg} alt=''/>
+          <img src={bannerImg} alt="" />
         </div>
       </section>
     </header>
   );
-}; 
+};
+
 export default Header;
